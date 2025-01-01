8e1e935383fab8ff29d14a354e1b94d052f4fcad
Bluewick <64674852+Bluewick@users.noreply.github.com>
2025-01-01 22:20:35 +0530

    create header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { FaSearch } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+
+export default function Header() {
+  return (
+    <header className="bg-slate-900 text-white">
+      <div className='flex justify-between items-center max-w-6xl mx-auto p-4'>
+        <Link to="/">
+        <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+          <span className="text-slate-500">REAL</span>
+          <span className="text-slate-700">Estate</span>
+        </h1>
+        </Link>
+        <form className='bg-slate-800 p-3 rounded-md flex items-center' action="">
+          <input type="text" placeholder="Search..." className='bg-transparent focus:outline-none w-24 sm:w-64'/>
+                <FaSearch className='text-slate-600' />
+        </form>
+        <ul className='flex gap-4'>
+
+                <li className='hidden sm:inline text-slate-300 hover:underline'>
+                    <Link to="/">Home</Link>
+                </li>
+                <li className='hidden sm:inline text-slate-300 hover:underline'>
+                    <Link to="/about">About</Link>
+                </li>
+                <li className='text-slate-300 hover:underline'>
+                    <Link to="/signin">Sign In</Link>
+                </li>
+                <li className='hidden sm:inline text-slate-300 hover:underline'>
+                    <Link to="/signup">Sign Up</Link>
+                </li>
+                <li className='hidden sm:inline text-slate-300 hover:underline'>
+                    <Link to="/profile">Profile</Link>
+                </li>
+        </ul>
+      </div>
+    </header>
+  );
+}
