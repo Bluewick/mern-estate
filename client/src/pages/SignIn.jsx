2c6e70ae12849e967251ae0bbce413eacaeef89f
Bluewick <64674852+Bluewick@users.noreply.github.com>
2025-01-06 03:07:22 +0530

    add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from 'react-redux';
+import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice';
 
 export default function SignIn() {
   const [formData, setFormData] = React.useState({ email: '', password: '' });
-  const [error, setError] = React.useState(null);
-  const [loading, setLoading] = React.useState(false);
+  // const [error, setError] = React.useState(null);
+  // const [loading, setLoading] = React.useState(false);
+  const { error, loading } = useSelector((state) => state.user);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
   };
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setLoading(true);
+    // setLoading(true);
+    dispatch(signInStart());
     try {
       const response = await fetch('/api/auth/signin', {
         method: 'POST',
@@ -22,16 +27,19 @@ export default function SignIn() {
       });
       const data = await response.json();
       if (data.success === false) {
-        setError(data.message);
-        setLoading(false);
+        // setError(data.message);
+        // setLoading(false);
+        dispatch(signInFailure(data.message));
         return;
       }
-      setLoading(false); 
-      setError(null);
+      // setLoading(false); 
+      // setError(null);
+      dispatch(signInSuccess(data));
       navigate('/');
     } catch (error) {
-      setLoading(false);
-      setError('An error occurred. Please try again later.');
+      // setLoading(false);
+      // setError('An error occurred. Please try again later.');
+      dispatch(signInFailure(error.message));
       console.error(error);
     }
   };
